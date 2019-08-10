a64d06cda2c1a5bb2b88ee97780a84d40db08534
James Ding <james.ding.illinois@gmail.com>
2019-08-10 18:36:59 -0500

    calculator update

--- src/main.c
+++ src/main.c
@@ -1,44 +1,66 @@
 #include <gtk/gtk.h>
 
-static void
-print_hello (GtkWidget *widget,
-             gpointer   data)
-{
-  g_print ("Hello World\n");
+GtkWidget *entry_alpha;
+GtkWidget *entry_beta;
+GtkWidget *entry_sigma1;
+GtkWidget *entry_sigma2;
+GtkWidget *entry_rho;
+
+GtkWidget *entry_zcbp;
+
+static void do_calibrate (GtkWidget *widget, gpointer data) { g_print ("Calibrating...Done\n"); }
+static void do_calculate (GtkWidget *widget, gpointer data) {
+    double alpha = atof((char *) gtk_entry_get_text (GTK_ENTRY(entry_alpha)));
+    double beta = atof((char *) gtk_entry_get_text (GTK_ENTRY(entry_beta)));
+    double sigma1 = atof((char *) gtk_entry_get_text (GTK_ENTRY(entry_sigma1)));
+    double sigma2 = atof((char *) gtk_entry_get_text (GTK_ENTRY(entry_sigma2)));
+    double rho = atof((char *) gtk_entry_get_text (GTK_ENTRY(entry_rho)));
+
+    char result_buffer[12];
+    snprintf(result_buffer, sizeof(result_buffer), "%f", alpha + beta + sigma1 + sigma2 + rho);
+    gtk_entry_set_text(GTK_ENTRY(entry_zcbp), result_buffer);
 }
 
-static void
-activate (GtkApplication *app, gpointer user_data)
+int main (int argc, char **argv)
 {
-  GtkWidget *button;
+
+  GtkBuilder *builder;
   GtkWidget *window;
-  GtkWidget *button_box;
+  GError *error = NULL;
 
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "GNOME HELLO WORLD");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_init (&argc, &argv);
 
-  button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
-  gtk_container_add (GTK_CONTAINER (window), button_box);
+  /* Construct a GtkBuilder instance and load our UI description */
+  builder = gtk_builder_new ();
+  if (gtk_builder_add_from_file (builder, "../ui/calculator.ui", &error) == 0)
+  {
+      g_printerr ("Error loading file: %s\n", error->message);
+      g_clear_error (&error);
+      return 1;
+  }
 
-  button = gtk_button_new_with_label ("Hello World");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-  g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
-  gtk_container_add (GTK_CONTAINER (button_box), button);
+  /* Connect signal handlers to the constructed widgets. */
+  window = (GtkWidget *)gtk_builder_get_object (builder, "window");
+  g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+
+  GObject *button_cali = gtk_builder_get_object (builder, "button_calibrate");
+  g_signal_connect (button_cali, "clicked", G_CALLBACK (do_calibrate), NULL);
+
+  GObject *button_calc = gtk_builder_get_object (builder, "button_calculate");
+  g_signal_connect (button_calc, "clicked", G_CALLBACK (do_calculate), NULL);
+
+  GObject *button_quit = gtk_builder_get_object (builder, "quit");
+  g_signal_connect (button_quit, "clicked", G_CALLBACK (gtk_main_quit), NULL);
+
+  entry_alpha = (GtkWidget *)gtk_builder_get_object (builder, "entry_alpha");
+  entry_beta = (GtkWidget *)gtk_builder_get_object (builder, "entry_beta");
+  entry_sigma1 = (GtkWidget *)gtk_builder_get_object (builder, "entry_sigma1");
+  entry_sigma2 = (GtkWidget *)gtk_builder_get_object (builder, "entry_sigma2");
+  entry_rho = (GtkWidget *)gtk_builder_get_object (builder, "entry_rho");
+  entry_zcbp = (GtkWidget *)gtk_builder_get_object (builder, "entry_zcbp");
 
   gtk_widget_show_all (window);
-}
+  gtk_main ();
 
-int
-main (int argc, char **argv)
-{
-    GtkApplication *app;
-    int status;
-
-    app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-    status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
-    return (status);
-
+  return 0;
 }
